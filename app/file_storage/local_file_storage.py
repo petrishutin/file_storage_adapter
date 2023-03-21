4d9588bcc2ccff948fcbdfcaf967d753a662364f
petr <petr.ishutin@gmail.com>
2023-03-21 11:10:38 +0100

    local_file_storage.py

---
+++ app/file_storage/local_file_storage.py
@@ -0,0 +1,36 @@
+import logging
+import os
+import pathlib
+
+from app.config import Config
+from app.file_storage.base_file_storage import FileStorage
+
+
+class LocalFileStorage(FileStorage):
+    def __init__(self, service_settings: Config):
+        super().__init__(service_settings)
+        self.storage_dir: str = service_settings.LOCAL_FILE_STORAGE_DIR
+        if not os.path.exists(self.storage_dir):
+            path = pathlib.Path(self.storage_dir)
+            path.mkdir(parents=True)
+
+    def _create_buket_if_not_exist(self, bucket):
+        path_to_bucket = self.storage_dir + f"/{bucket}"
+        if not os.path.exists(path_to_bucket):
+            path = pathlib.Path(path_to_bucket)
+            path.mkdir(parents=True)
+
+    async def upload(self, bucket_name: str, filename: str, file_data: bytes) -> bool:
+        self._create_buket_if_not_exist(bucket_name)
+        try:
+            with open(f"{self.storage_dir}/{bucket_name}/{filename}", "wb+") as file:
+                file.write(file_data)
+            return True
+        except Exception as e:
+            logging.error(f"Can not save file {filename}. Error {e}")
+            return False
+
+    async def download(self, bucket_name: str, filename: str) -> bytes:
+        with open(f"{self.storage_dir}/{bucket_name}/{filename}", "rb") as file:
+            r = file.read()
+            return r
